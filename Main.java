5a42881b9fffe64f573c14b6bbaba7c2d15e9810
Ruben <rubenbaloncesto10@gmail.com>
2023-10-10 09:47:34 +0200

    Main prints

--- Main.java
+++ Main.java
@@ -2,5 +2,5 @@ System.outprintln(farewell);public static void Main(String[]args){
 String solutation="Hola mundo";
 System.outprintln(salutation);
 String farewell = "Adios con el corazon";
-System.outprintln(farewell)+solutation;
+System.outprintln(farewell+solutation;)
 }
